e5673f9ebc07c2aa06f13de74f6408a3db25e6c1
FrankBO <slipmip@outlook.dk>
2021-09-24 22:23:13 +0200

    finished bouncing ball project and yet im improvin

--- Bouncingball/sketch.js
+++ Bouncingball/sketch.js
@@ -1,4 +1,9 @@
-let xPos = 400
+
+let xPos = 100
+let yPos = 100
+
+let xSpeed = 2
+let ySpeed = 2
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,4 +11,36 @@ function setup() {
 
 function draw() {
   background(220);
+
+  let width = 50
+  let height = 30
+  // rect(xPos,yPos,width,height)
+
+let img;
+function preload() {
+  img = loadima('bouncingball/bootlegdvd.png')
+}
+function setup() {
+image(img, xPos, yPos, width, height)
+}
+
+xPos = xPos + xSpeed
+yPos = yPos + ySpeed
+
+// in this case b = beyond
+// "bleft = beyond left"
+let bLeft = xPos < 0
+let bRight = xPos > windowWidth - width
+let bBottom = yPos > windowHeight - height
+let bTop = yPos < 0
+
+if (bLeft || bRight) {
+xSpeed = -xSpeed
+}
+
+if (bBottom || bTop) {
+  ySpeed = -ySpeed
+}
+
+
 }
