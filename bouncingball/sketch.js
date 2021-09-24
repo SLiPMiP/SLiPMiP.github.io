49a38737cb21f54c3626a10f9e9fa93c7463977d
FrankBO <slipmip@outlook.dk>
2021-09-25 00:24:59 +0200

    new bouncingball folder works

--- bouncingball/sketch.js
+++ bouncingball/sketch.js
@@ -1,7 +1,35 @@
+let xPos = 100;
+let yPos = 100;
+
+let xSpeed = 2;
+let ySpeed = 2;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+
+  let width = 50;
+  let height = 30;
+  rect(xPos,yPos,width,height)
+
+  xPos = xPos + xSpeed;
+  yPos = yPos + ySpeed;
+
+  // in this case b = beyond
+  // "bleft = beyond left"
+  let bLeft = xPos < 0;
+  let bRight = xPos > windowWidth - width;
+  let bBottom = yPos > windowHeight - height;
+  let bTop = yPos < 0;
+
+  if (bLeft || bRight) {
+    xSpeed = -xSpeed;
+  }
+
+  if (bBottom || bTop) {
+    ySpeed = -ySpeed;
+  }
 }
