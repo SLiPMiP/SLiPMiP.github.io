d72b1ae1a84b66a3d62fd4f8ea39652b45023bcd
FrankBO <slipmip@outlook.dk>
2021-09-25 00:05:47 +0200

    finished logical operators task

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 Her er en oversigt over projekter
 
-- [Opskrift](opskrift/)
 - [My manstick](stickman/)
+- [Opskrift](opskrift/)
+- [Bouncingball](bouncingball/)
+- [logical operators](logicaloperators/)
 
 ## Om mig
---
+++ logicaloperators/sketch.js
@@ -0,0 +1,50 @@
+let xPos = 0;
+let yPos = 0;
+
+let xSpeed = 2;
+let ySpeed = 2;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  let width = 20;
+  let height = 20;
+  strokeWeight(1);
+  rect(xPos, yPos, width, height);
+
+  if (
+    (xPos > windowWidth / 2 - width / 2 && yPos < windowHeight / 2 - height / 2) ||
+    (xPos < windowWidth / 2 - width / 2 && yPos > windowHeight / 2 - height / 2)
+  ) {
+    fill("red");
+  } else {
+    fill("white");
+  }
+
+  strokeWeight(width);
+  line(windowWidth / 2, 0, windowWidth / 2, windowHeight);
+  strokeWeight(height);
+  line(0, windowHeight / 2, windowWidth, windowHeight / 2);
+
+  xPos = xPos + xSpeed;
+  yPos = yPos + ySpeed;
+
+  // in this case b = beyond
+  // "bleft = beyond left"
+  let bLeft = xPos < 0;
+  let bRight = xPos > windowWidth - width;
+  let bBottom = yPos > windowHeight - height;
+  let bTop = yPos < 0;
+
+  if (bLeft || bRight) {
+    xSpeed = -xSpeed;
+  }
+
+  if (bBottom || bTop) {
+    ySpeed = -ySpeed;
+  }
+}
