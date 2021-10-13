390c349fae162150cc4e28206dad46f0ac52edab
slipmip <slipmip@outlook.dk>
2021-10-13 23:32:44 +0200

    dunno

--- loops/loop1/sketch.js
+++ loops/loop1/sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+    background(220);
+
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+
 }
